70be8be059aa5285eecb73c75cdb4681526d4309
lizzaaabb <lbogveradze12@gmail.com>
2026-02-24 18:39:45 +0400

    removed hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import '../styles/HeroSection.css'
 
-const blur1   = process.env.PUBLIC_URL + '/pics/blur.png'
-const blur2   = process.env.PUBLIC_URL + '/pics/blur2.png'
-const circles = process.env.PUBLIC_URL + '/pics/circles-background.png'
-const web     = process.env.PUBLIC_URL + '/pics/web3.png'
+const blur1 = process.env.PUBLIC_URL + '/pics/blur.png'
+const web   = process.env.PUBLIC_URL + '/pics/web3.png'
 
 function HeroSection() {
   return (
     <div className='hero-section-container'>
-      <div className="hs-images">
-        <img src={blur1}   alt="" className="hs-blur1"   aria-hidden="true" />
+
+      <img src={blur1} alt="" className="hs-blur1" aria-hidden="true" />
+
+      <div className="hero-content">
+        <h1 className="hero-title">საიტის აწყობა</h1>
+        <h1 className="hero-title2">ვებ გვერდების დამზადება</h1>
+        <p className="hero-subtitle">ვებ საიტის დამზადება მარტივად და სწრაფად</p>
+        <a href="#contact" className="hero-cta">დაგვიკავშირდით</a>
       </div>
-     <div className="hs-web">
-  <img src={web} alt="" className="hs-blur2" aria-hidden="true" />
-  <div className="hs-web-fade" />
-</div>
+
+      <div className="hs-web">
+        <img src={web} alt="" className="hs-blur2" aria-hidden="true" />
+        <div className="hs-web-fade" />
+      </div>
+
     </div>
   )
 }
